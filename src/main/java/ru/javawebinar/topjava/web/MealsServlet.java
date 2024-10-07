7a7a04592940de2734d56e88735e43431e39e3cc
Ilya Erulaev <ilyaerulaev@gmail.com>
2024-10-07 14:38:45 +0400

    Meals CRUD implementation

--- src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
@@ -17,98 +17,36 @@ import java.time.Month;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealsServlet extends HttpServlet {
-    private final Storage storage = new MemoryMapStorage();
+    private static final Storage STORAGE = new MemoryMapStorage();
     private static final Logger log = getLogger(MealsServlet.class);
     private static final String ADD_OR_UPDATE = "/editMeal.jsp";
     private static final String LIST_MEALS = "/meals.jsp";
 
-    {
-        storage.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
-        storage.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
-        storage.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
-        storage.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
-        storage.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
-        storage.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
-        storage.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
+    //data for test:
+    static {
+        STORAGE.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
+        STORAGE.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
+        STORAGE.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
+        STORAGE.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
+        STORAGE.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
+        STORAGE.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
+        STORAGE.add(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String forward;
-//        String action = req.getParameter("action");
-//
-//        if (action == null) {
-//            forward = LIST_MEALS;
-//            req.setAttribute("mealsTo", MealsUtil.getTo(storage.getAll(), MealsUtil.CALORIES_PER_DAY));
-//        } else {
-//            if (action.equals("delete")) {
-//                forward = LIST_MEALS;
-//                storage.delete(Integer.parseInt(req.getParameter("id")));
-//                req.setAttribute("mealsTo", MealsUtil.getTo(storage.getAll(), MealsUtil.CALORIES_PER_DAY));
-//            } else {
-//                forward = ADD_OR_UPDATE;
-//                String mealId = req.getParameter("id");
-//                if (mealId == null) {
-//                    req.setAttribute("dateTime", LocalDateTime.now());
-//                    req.setAttribute("description", "");
-//                    req.setAttribute("calories", 0);
-//                    req.setAttribute("id", -1);
-//                } else {
-//                    Meal meal = storage.get(Integer.parseInt(mealId));
-//                    req.setAttribute("dateTime", meal.getDateTime());
-//                    req.setAttribute("description", meal.getDescription());
-//                    req.setAttribute("calories", meal.getCalories());
-//                    req.setAttribute("id", meal.getId());
-//                }
-//            }
-//        }
-//        req.getRequestDispatcher(forward).forward(req, resp);
-
-
-//        String forward;
-//        String action = req.getParameter("action");
-//
-//        if (action == null) {
-//            forward = LIST_MEALS;
-//            req.setAttribute("mealsTo", MealsUtil.getTo(storage.getAll(), MealsUtil.CALORIES_PER_DAY));
-//            req.getRequestDispatcher(forward).forward(req, resp);
-//        } else {
-//            if (action.equals("delete")) {
-//                //forward = LIST_MEALS;
-//                storage.delete(Integer.parseInt(req.getParameter("id")));
-//                req.setAttribute("mealsTo", MealsUtil.getTo(storage.getAll(), MealsUtil.CALORIES_PER_DAY));
-//                resp.sendRedirect(req.getRequestURI());
-//            } else {
-//                forward = ADD_OR_UPDATE;
-//                String mealId = req.getParameter("id");
-//                if (mealId == null) {
-//                    req.setAttribute("dateTime", LocalDateTime.now());
-//                    req.setAttribute("description", "");
-//                    req.setAttribute("calories", 0);
-//                    req.setAttribute("id", -1);
-//                } else {
-//                    Meal meal = storage.get(Integer.parseInt(mealId));
-//                    req.setAttribute("dateTime", meal.getDateTime());
-//                    req.setAttribute("description", meal.getDescription());
-//                    req.setAttribute("calories", meal.getCalories());
-//                    req.setAttribute("id", meal.getId());
-//                }
-//                req.getRequestDispatcher(forward).forward(req, resp);
-//            }
-//        }
-        String forward;
         String action = req.getParameter("action");
 
         if (action == null) {
-            forward = LIST_MEALS;
-            req.setAttribute("mealsTo", MealsUtil.getTo(storage.getAll(), MealsUtil.CALORIES_PER_DAY));
-            req.getRequestDispatcher(forward).forward(req, resp);
+            req.setAttribute("mealsTo", MealsUtil.getTo(STORAGE.getAll(), MealsUtil.CALORIES_PER_DAY));
+            log.debug("forward to meals.jsp");
+            req.getRequestDispatcher(LIST_MEALS).forward(req, resp);
         } else {
             if (action.equals("delete")) {
-                storage.delete(Integer.parseInt(req.getParameter("id")));
+                STORAGE.delete(Integer.parseInt(req.getParameter("id")));
+                log.debug("redirect to MealsServlet after delete");
                 resp.sendRedirect(req.getRequestURI());
-            } else {
-                forward = ADD_OR_UPDATE;
+            } else if (action.equals("edit")) {
                 String mealId = req.getParameter("id");
                 if (mealId == null) {
                     req.setAttribute("dateTime", LocalDateTime.now());
@@ -116,13 +54,16 @@ public class MealsServlet extends HttpServlet {
                     req.setAttribute("calories", 0);
                     req.setAttribute("id", -1);
                 } else {
-                    Meal meal = storage.get(Integer.parseInt(mealId));
+                    Meal meal = STORAGE.get(Integer.parseInt(mealId));
                     req.setAttribute("dateTime", meal.getDateTime());
                     req.setAttribute("description", meal.getDescription());
                     req.setAttribute("calories", meal.getCalories());
                     req.setAttribute("id", meal.getId());
                 }
-                req.getRequestDispatcher(forward).forward(req, resp);
+                log.debug("forward to editMeal.jsp");
+                req.getRequestDispatcher(ADD_OR_UPDATE).forward(req, resp);
+            } else {
+                resp.sendRedirect(req.getRequestURI());
             }
         }
     }
@@ -138,13 +79,15 @@ public class MealsServlet extends HttpServlet {
             calories = 0;
         }
         if (id < 1) {
-            storage.add(new Meal(LocalDateTime.parse(req.getParameter("dateTime")),
+            STORAGE.add(new Meal(LocalDateTime.parse(req.getParameter("dateTime")),
                     req.getParameter("description"), calories));
+            log.debug("new meal added");
         } else {
-            storage.update(new Meal(LocalDateTime.parse(req.getParameter("dateTime")),
+            STORAGE.update(new Meal(LocalDateTime.parse(req.getParameter("dateTime")),
                     req.getParameter("description"), calories, id));
+            log.debug("meal with id " + id + " updated");
         }
-        req.setAttribute("mealsTo", MealsUtil.getTo(storage.getAll(), MealsUtil.CALORIES_PER_DAY));
+        req.setAttribute("mealsTo", MealsUtil.getTo(STORAGE.getAll(), MealsUtil.CALORIES_PER_DAY));
         req.getRequestDispatcher(LIST_MEALS).forward(req, resp);
     }
 }
--- src/main/webapp/editMeal.jsp
+++ src/main/webapp/editMeal.jsp
@@ -8,16 +8,26 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Edit Meal</h2>
-<form action='meals' method="post" accept-charset="utf-8">
+<form action='meals' method="post">
     <jsp:useBean id="dateTime" scope="request" type="java.time.LocalDateTime"/>
     <jsp:useBean id="description" scope="request" type="java.lang.String"/>
     <jsp:useBean id="calories" scope="request" type="java.lang.Integer"/>
     <jsp:useBean id="id" scope="request" type="java.lang.Integer"/>
-    <p>DateTime:<input type="datetime-local" name="dateTime" value="${dateTime.format(MealsUtil.DATE_TIME_FORMATTER)}"/></p>
-    <p>Description:<input type="text" name="description" maxlength="300" value="${description}"/></p>
-    <p>Calories:<input type="text" name="calories" minlength="1" maxlength="9" pattern="[0-9]+" value="${calories}"/></p>
+    <p style="background-color: ghostwhite">DateTime:<input type="datetime-local" name="dateTime"
+                                                            style="margin-left: 30px"
+                                                            value="${dateTime.format(MealsUtil.DATE_TIME_FORMATTER)}"/>
+    </p>
+    <p style="background-color: ghostwhite">Description:<input type="text" name="description" maxlength="300"
+                                                               style="margin-left: 19px" size="40"
+                                                               value="${description}"/></p>
+    <p style="background-color: ghostwhite">Calories:<input type="text" name="calories" minlength="1" maxlength="9"
+                                                            pattern="[0-9]+" style="margin-left: 39px"
+                                                            value="${calories}"/>
+    </p>
     <input type="hidden" name="id" value="${id}"/>
-    <p><input type="submit" value="Save"/><button onclick="window.history.back()" type="button">Cancel</button>
+    <p><input type="submit" value="Save"/>
+        <button onclick="window.history.back()" type="button" style="margin-left: 10px">Cancel</button>
+    </p>
 </form>
 </body>
 </html>
