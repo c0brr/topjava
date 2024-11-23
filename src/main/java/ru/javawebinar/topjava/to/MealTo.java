6e1616170fc6bf0a2bf3aa2d839dc0eaabc4a8b7
Ilya Erulaev <ilyaerulaev@gmail.com>
2024-11-23 16:29:35 +0400

    HW7 MealRestController, test impl

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.to;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
@@ -16,7 +18,12 @@ public class MealTo {
 
     private final boolean excess;
 
-    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+    @JsonCreator
+    public MealTo(@JsonProperty("id") Integer id,
+                  @JsonProperty("dateTime") LocalDateTime dateTime,
+                  @JsonProperty("description") String description,
+                  @JsonProperty("calories") int calories,
+                  @JsonProperty("excess") boolean excess) {
         this.id = id;
         this.dateTime = dateTime;
         this.description = description;
